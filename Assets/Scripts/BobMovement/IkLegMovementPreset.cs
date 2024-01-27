cd5788f54000993485006dd39dcc5f641610db21
69VR69 <44296242+69VR69@users.noreply.github.com>
2024-01-27 13:18:00 +0100

    Walking without gravity

---
+++ Assets/Scripts/BobMovement/IkLegMovementPreset.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+namespace Assets.Scripts.BobMovement
+{
+    [CreateAssetMenu(fileName = "IkLegMovement", menuName = "BobRagdoll/IkLegMovement")]
+    public class IkLegMovementPreset : ScriptableObject
+    {
+        public float StepLength = 0.3f;
+        public float StepThreshold = 0.6f;
+        public float StepHeight = 0.3f;
+        public float StepDuration = 1f;
+        public AnimationCurve StepSpeedCurve = null;
+    }
+}
