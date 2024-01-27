7c7234a59dfe803e79a1e30a44c3a1ca687ae5a6
69VR69 <44296242+69VR69@users.noreply.github.com>
2024-01-27 18:06:59 +0100

    Add movements to Bob

--- Assets/Scripts/BobMovement/BobMovement.cs
+++ Assets/Scripts/BobMovement/BobMovement.cs
@@ -1,11 +1,46 @@
 ﻿using System.Collections;
 
+using Assets.Scripts.BobMovement;
+
 using UnityEngine;
 
 namespace Assets
 {
     public class BobMovement : MonoBehaviour
     {
+        [SerializeField]
+        private float _speed = 1.0f;
+        [SerializeField]
+        private IkRig _ikRig;
+
+        private void Awake()
+        {
+            if (_ikRig == null)
+                Debug.LogError("IkRig not assigned");
+        }
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.K))
+                MoveInDirection(transform.forward);
+
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                _ikRig.LegPreset.StepDuration /= _speed;
+                _ikRig.ShoulderPreset.StepDuration /= _speed;
+            }
+            else if (Input.GetKeyUp(KeyCode.K))
+            {
+                _ikRig.LegPreset.StepDuration *= _speed;
+                _ikRig.ShoulderPreset.StepDuration *= _speed;
+            }
+        }
+
+        private void MoveInDirection(Vector3 direction)
+        {
+            Vector3 force = direction * _speed * Time.deltaTime;
+            transform.position += force;
+        }
 
     }
 }
