5d04f55cba7b8bc5fb627a6a3183174f6a2d7277
69VR69 <44296242+69VR69@users.noreply.github.com>
2024-01-27 18:06:59 +0100

    Add movements to Bob

--- Assets/Scripts/BobMovement/IkRig.cs
+++ Assets/Scripts/BobMovement/IkRig.cs
@@ -9,11 +9,17 @@ namespace Assets.Scripts.BobMovement
     {
         #region Variable Field
         [SerializeField] private IkLowerBody[] _lowerBodies = null;
+        [SerializeField] private IkUpperBody[] _upperBodies = null;
         [SerializeField] private IkLegMovementPreset _legPreset = null;
+        [SerializeField] private IkLegMovementPreset _shoulderPreset = null;
+
+        public IkLegMovementPreset LegPreset => _legPreset;
+        public IkLegMovementPreset ShoulderPreset => _shoulderPreset;
 
         private float _rearDelay = 0.75f;
 
         private IEnumerator _lowerBodyMovement = null;
+        private IEnumerator _upperBodyMovement = null;
 
         #endregion
 
@@ -21,12 +27,16 @@ namespace Assets.Scripts.BobMovement
         {
             for (int i = 0; i < _lowerBodies.Length; i++)
                 _lowerBodies[i].Init();
+            for (int i = 0; i < _upperBodies.Length; i++)
+                _upperBodies[i].Init();
         }
 
         private void Start()
         {
             _lowerBodyMovement = LowerBodyMovement();
             StartCoroutine(_lowerBodyMovement);
+            _upperBodyMovement = UpperBodyMovement();
+            StartCoroutine(_upperBodyMovement);
         }
 
         #region Private Field
@@ -41,6 +51,24 @@ namespace Assets.Scripts.BobMovement
                 if (elapsedTime > delay)
                 {
                     _lowerBodies[index++ % _lowerBodies.Length].MoveLegs(_legPreset);
+                    elapsedTime = 0;
+                }
+                elapsedTime += Time.fixedDeltaTime;
+
+                yield return new WaitForFixedUpdate();
+            }
+        }
+        private IEnumerator UpperBodyMovement()
+        {
+            float elapsedTime = 0f;
+            int index = 0;
+
+            while (true)
+            {
+                float delay = _shoulderPreset.StepDuration * _rearDelay;
+                if (elapsedTime > delay)
+                {
+                    _upperBodies[index++ % _upperBodies.Length].MoveLegs(_shoulderPreset);
                     elapsedTime = 0;
                 }
                 elapsedTime += Time.fixedDeltaTime;
---
+++ Assets/Scripts/BobMovement/IkUpperBody.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+
+using UnityEngine;
+
+namespace Assets.Scripts.BobMovement
+{
+    public class IkUpperBody : MonoBehaviour
+    {
+
+        #region Variable Field
+        [SerializeField] private Transform _body = null;
+        [SerializeField] private IkShoulder _leftShoulder = null;
+        [SerializeField] private IkShoulder _rightShoulder = null;
+        [SerializeField] private DominentLeg _dominentLeg = DominentLeg.Left;
+
+        private IkShoulder _primaryShoulder = null;
+        private IkShoulder _secondaryShoulder = null;
+        private bool _legMoving = false;
+        private float _leftRightDelay = 0.5f;
+
+        private IEnumerator _legMovement = null;
+
+        #endregion
+
+        #region Public Functions
+        public void Init()
+        {
+            _leftShoulder.Init();
+            _rightShoulder.Init();
+
+            if (_dominentLeg == DominentLeg.Left)
+            {
+                _primaryShoulder = _leftShoulder;
+                _secondaryShoulder = _rightShoulder;
+            }
+            else
+            {
+                _primaryShoulder = _rightShoulder;
+                _secondaryShoulder = _leftShoulder;
+            }
+        }
+
+        public void MoveLegs(IkLegMovementPreset preset)
+        {
+            if (_legMoving)
+                return;
+            _legMoving = true;
+
+            _legMovement = LegMovement(preset, () =>
+            {
+                _legMoving = false;
+            });
+            StartCoroutine(_legMovement);
+        }
+
+        #endregion
+
+        #region Private Functions
+        private IEnumerator LegMovement(IkLegMovementPreset preset, Action done)
+        {
+            _primaryShoulder.MoveLeg(_body, preset);
+
+            float delay = preset.StepDuration * _leftRightDelay;
+            yield return new WaitForSeconds(delay);
+
+            _secondaryShoulder.MoveLeg(_body, preset);
+
+            done?.Invoke();
+        }
+
+        #endregion
+    }
+}
