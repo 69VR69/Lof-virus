5862107abd44598683db0143c641ce8e1077677d
Elyas TIGRE <elyastigre@gmail.com>
2024-01-28 16:49:27 +0100

    WIP

--- Assets/Scripts/Managers/TileManager.cs
+++ Assets/Scripts/Managers/TileManager.cs
@@ -38,6 +38,20 @@ public class TileManager : Singleton<TileManager>
         GenerateLevel(0);
     }
 
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GenerateLevel(CurrentLevel);
+        }
+        else if (Input.GetKeyDown(KeyCode.N))
+        {
+            var level = (CurrentLevel + 1) % LevelNumber;
+
+            GenerateLevel(level);
+        }
+    }
+
 
     public void GenerateLevel(int levelNumber)
     {
