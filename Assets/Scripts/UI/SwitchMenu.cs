12d50c854ad6b66ca4ef6fd643a46ac1c633a34f
Faadil Torou <faadil@lynx-business.com>
2024-01-28 17:21:19 +0100

    feat(main_menu): show main menu

--- Assets/Scripts/UI/SwitchMenu.cs
+++ Assets/Scripts/UI/SwitchMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SwitchMenu : MonoBehaviour
 {
@@ -20,5 +21,15 @@ public class SwitchMenu : MonoBehaviour
         }
         //TODO add Audio Effect here!!!
         menu_ui.gameObject.SetActive( true );
+    }
+
+    /**
+     * Function for switching to main game scene
+     * 
+     * @return void
+     */
+    public void SwitchToGame()
+    {
+        SceneManager.LoadScene("Main");
     }
 }
